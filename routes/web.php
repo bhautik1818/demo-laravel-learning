3f9f21eae0f89b0113903c20e26705a46db79700
bhautik1818 <bhautik@logisticinfotech.co.in>
2022-12-05 20:47:04 +0530

    login auth.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,18 @@
 |
 */
 
-Route::get('/', function () {
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
+// Route::get('/{about}', function ($about) {
+//     return view('about',['about'=>$about]);
+// });
+Route::get('/user/{id}', "UserController@show");
+// Route::view('about','/about');
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
